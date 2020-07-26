30844b4f963e777c1a46dfb4ecc0b86ec2bf8fcf
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-07-26 13:27:52 +0400

    configration steps images are added

--- lib/presentation/pages/welcome/welcome_page.dart
+++ lib/presentation/pages/welcome/welcome_page.dart
@@ -1,8 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:handy/data/repositories/auth_repository.dart';
 import '../../../utils/constants/app_strings.dart';
 import '../../shared/app_colors.dart';
 import '../../shared/app_icons.dart';
