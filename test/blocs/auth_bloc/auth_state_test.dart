7cbfc080b28b515853e8854e05037cfe4cf55f9b
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-08-09 14:48:03 +0400

    auth state test added

--- test/blocs/auth_bloc/auth_state_test.dart
+++ test/blocs/auth_bloc/auth_state_test.dart
@@ -1,1 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:handy/blocs/auth_bloc/auth_bloc.dart';
+import 'package:mockito/mockito.dart';
 
+class MockFirebaseUser extends Mock implements FirebaseUser {}
+
+final mockFirebaseUser = MockFirebaseUser();
+
+void main() {
+  group('AuhState', () {
+    group('Auth Initial', () {
+      test('supports value comparisons', () {
+        expect(
+          AuthInitial(),
+          AuthInitial(),
+        );
+      });
+    });
+
+    group('Auth Unauthenticated', () {
+      test('supports value comparisons', () {
+        expect(
+          Unauthenticated(),
+          Unauthenticated(),
+        );
+      });
+    });
+
+    group('Auth In Progress', () {
+      test('supports value comparisons', () {
+        expect(
+          AuthInProgress(),
+          AuthInProgress(),
+        );
+      });
+    });
+
+    group('Auth Authenticated', () {
+      test('supports value comparisons', () {
+        expect(
+          Authenticated(mockFirebaseUser),
+          Authenticated(mockFirebaseUser),
+        );
+      });
+    });
+  });
+
+  group('Auth Failure', () {
+    test('supports value comparisons', () {
+      expect(
+        AuthFailure('failure'),
+        AuthFailure('failure'),
+      );
+    });
+  });
+}
